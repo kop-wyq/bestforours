f2c91b3b07b23182ed820dfc645d9a8f7be9c81b
wyq <376858884@qq.com>
2020-03-19 00:51:26 +0800

    bootstrap

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,31 +1,12 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+
+class Index extends Controller
 {
     public function index()
     {
-        $str = <<<STR
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<style>
-    body {
-        background-image: url('static/images/20200318001838.jpg');
-        background-size: 500px 300px;
-    }
-</style>
-<body>
-    <div></div>
-</body>
-</html>
-STR;
-        echo $str;
+        return $this->fetch();
     }
 }
